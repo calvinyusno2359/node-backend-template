1ef862c93a18aaed672d7500c69f830074c18e26
calvinyusno2359 <calvin.yusnoveri@2359media.com>
2021-09-02 12:11:58 +0800

    added express app/server

---
+++ app.js
@@ -0,0 +1,28 @@
+"use-strict";
+
+const dotenv = require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+
+const app = new express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  express.json({
+    limit: "5mb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "5mb",
+  })
+);
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Listening to port: ${PORT}`);
+});
