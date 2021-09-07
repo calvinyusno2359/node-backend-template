942e615cb67e2cad9df939b6557cd31665497078
calvinyusno2359 <calvin.yusnoveri@2359media.com>
2021-09-07 11:39:15 +0800

    fixed model to autoincrement id

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -3,11 +3,6 @@
 const router = require("express").Router();
 const userRouter = require("./user");
 
-router.get("/", (req, res) => {
-  console.log("hello world");
-  res.send("hello world");
-});
-
 router.use("/users", userRouter);
 
 module.exports = router;
