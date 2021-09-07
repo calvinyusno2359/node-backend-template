7af48782a80096fe5c7bba822b8b92b4468b39b7
calvinyusno2359 <calvin.yusnoveri@2359media.com>
2021-09-07 12:05:40 +0800

    logIn now looks at params.id

--- src/api/user/logIn.js
+++ src/api/user/logIn.js
@@ -3,7 +3,7 @@
 const { userService } = require("../../services");
 
 async function logIn(req, res, next) {
-  const user = await userService.getUser(req.body);
+  const user = await userService.getUser(req.params);
   return res.json({ user });
 }
 
